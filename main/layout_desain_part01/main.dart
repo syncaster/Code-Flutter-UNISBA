2a46950e1b4801ccf1f9625b0230bc67e7701b20
Saiful Nur B <sync.saifulnb@gmail.com>
2023-05-26 07:18:51 +0700

    revisi screen.dart
    perbaikan coding pada return Scafold yang harus menyertakan body terhadap isi dari Container

--- main/layout_desain_part01/main.dart
+++ main/layout_desain_part01/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:layout_desain_part01/screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: null,
+      home: HelloScreen(),
     );
   }
 }
--- main/layout_desain_part01/screen.dart
+++ main/layout_desain_part01/screen.dart
@@ -1,2 +1,26 @@
-:) halo disini masih kosong ya.
-coding manual yuk, jangan copy paste saja :p
+import 'package:flutter/material.dart';
+
+class HelloScreen extends StatefulWidget {
+  const HelloScreen({super.key});
+
+  @override
+  State<HelloScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HelloScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var box = Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.blue, Colors.red])),
+      // color: Colors.red,
+      margin: EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(16.0),
+      child: Text('Hello Flutter'),
+    );
+
+    return Scaffold(
+      body: box,
+    );
+  }
+}
